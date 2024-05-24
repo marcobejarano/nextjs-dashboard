e30030af8efd2d6c484a73bfc4a8bd7cbc929dba
Marco <m_uno_p@hotmail.com>
2024-05-24 16:31:35 -0500

    Improve accessibility

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { 
   CheckIcon,
   ClockIcon,
@@ -5,6 +7,7 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useFormState } from "react-dom";
 import { Button } from "../button";
 import { CustomerField } from "@/app/lib/definitions";
 import { createInvoice } from "@/app/lib/actions";
@@ -14,8 +17,11 @@ const Form = ({
 }: {
   customers: CustomerField[]
 }) => {
+  const initialState = { message: "", errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+
   return (
-    <form action={ createInvoice }>
+    <form action={ dispatch }>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -28,6 +34,7 @@ const Form = ({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -39,6 +46,14 @@ const Form = ({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
