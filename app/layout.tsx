131582f8379db50ecdb7479f379209dd53e7bd6e
Marco <m_uno_p@hotmail.com>
2024-05-24 20:42:01 -0500

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import { ReactNode } from 'react';
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 const RootLayout = ({
   children
